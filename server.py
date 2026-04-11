a4118d610ee7b6496991bb05f49c53cd0dae90af
Yoav Moratt <ymoratt@gmail.com>
2026-04-11 12:56:25 +0300

    Fixed server host and port

--- server.py
+++ server.py
@@ -435,5 +435,6 @@ def admin_delete_user(user_id):
 
 if __name__ == '__main__':
     init_db()
-    print('השרת פועל על http://localhost:5000')
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    print(f'השרת פועל על http://localhost:{port}')
+    app.run(host='0.0.0.0', port=port, debug=False)
